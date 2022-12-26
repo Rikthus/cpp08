95babf5c2204c8af7da654ad1d3f9ec73273630c
maxperei <maxperei@student.42lyon.fr>
2022-12-26 05:38:46 +0100

    Ex02 finally found how to make canonical orthodox form form inheriting class

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -1,7 +1,32 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# include <iostream>
+# include <stack>
+
 template< typename T >
-class	MutantStack : std::stack
+class	MutantStack : public std::stack<T>
+{
+	public:
+
+		
+
+		MutantStack(MutantStack const & src) : std::stack<T>(src)
+		{
+		};
+		MutantStack(void)
+		{
+			std::stack<T>();
+		};
+		~MutantStack(void)
+		{
+		};
+		MutantStack &	operator=(MutantStack const & rhs)
+		{
+			if (this != &rhs)
+				std::stack<T>::operator=(rhs);
+			return (*this);
+		}
+};
 
 #endif
--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -2,6 +2,19 @@
 
 int	main(void)
 {
+	MutantStack<int>	mStack;
+	MutantStack<int>	assignationStack;
+
+	mStack.push(5);
+	mStack.push(17);
+	std::cout << mStack.top() << std::endl;
+	mStack.pop();
+	std::cout << mStack.size() << std::endl;
+	MutantStack<int>	cpStack(mStack);
+	std::cout << cpStack.top() << std::endl;
+
+	assignationStack = cpStack;
+	std::cout << assignationStack.top() << std::endl;
 
 	return (0);
 }
