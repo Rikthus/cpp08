b68c418dff9210a33803564bfa3001c389e3bb23
maxperei <maxperei@student.42lyon.fr>
2022-12-26 05:46:20 +0100

    Enhance tests for ex02

--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -5,14 +5,24 @@ int	main(void)
 	MutantStack<int>	mStack;
 	MutantStack<int>	assignationStack;
 
+	std::cout << std::endl;
+	std::cout << "Testing stack member functions: " << std::endl;
+	std::cout << "Is mutant stack empty ? " << mStack.empty() << std::endl;
 	mStack.push(5);
+	std::cout << "Is mutant stack empty ? " << mStack.empty() << std::endl;
+	std::cout << std::endl;
 	mStack.push(17);
 	std::cout << mStack.top() << std::endl;
 	mStack.pop();
-	std::cout << mStack.size() << std::endl;
+	std::cout << std::endl;
+	std::cout << "Size of the mutant stack " << mStack.size() << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Testing copy constructor: ";
 	MutantStack<int>	cpStack(mStack);
 	std::cout << cpStack.top() << std::endl;
 
+	std::cout << "Testing assignation operator: ";
 	assignationStack = cpStack;
 	std::cout << assignationStack.top() << std::endl;
 
